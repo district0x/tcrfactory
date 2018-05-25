791e3c8f29db23ab56f497bfecacc981ccfa6615
Juan Monetta <jpmonettas@gmail.com>
2018-05-25 12:36:26 -0300

    Add search-registries-resolver

--- src/tcrfactory/server/graphql_resolvers.cljs
+++ src/tcrfactory/server/graphql_resolvers.cljs
@@ -11,7 +11,8 @@
             [taoensso.timbre :as log]
             [tcrfactory.server.db :as meme-db]
             [print.foo :include-macros true :refer [look]]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [district.server.graphql :refer [run-query]]))
 
 (defn registry-entries-resolver [args context document]
 (log/info "registry-entries resolver args" args)
@@ -24,5 +25,43 @@
                   :where [:= address :registries.registry/address]})
          {:registry/entries (registry-entries-resolver args context document)}))
 
+(defn search-registries-resolver [{:keys [keyword] :as args} context document]
+  (log/info ":registry " args)
+  (db/all {:select [:*]
+           :from [:registries]
+           :where [:or
+                   [:like (sql/call :upper :registries.registry/title) (str/upper-case (str "%" keyword "%"))]
+                   [:like (sql/call :upper :registries.registry/description) (str/upper-case (str "%" keyword "%"))]]}))
+
 (def graphql-resolvers
-  {:registry registry-resolver})
+
+  {:registry registry-resolver 
+   :search-registries search-registries-resolver }))
+
+;;;;;;;;;;;
+;; Tests ;;
+;;;;;;;;;;;
+
+(comment
+
+
+  (db/get {:select [:*]
+                        :from [:registries]
+           :where [:= "0x1024477d9d41bc2d4740b152f4f847b8f805cf3a" :registries.registry/address]})
+  
+  (run-query {:queries [[:registry {:registry/address "0x1024477d9d41bc2d4740b152f4f847b8f805cf3a"}
+                         [:registry/address
+                          :registry/title]]]})
+
+  (run-query {:queries [[:search-registries {:keyword "list"}
+                         [:registry/address
+                          :registry/title
+                          :registry/description]]]})
+
+  (run-query {:queries [[:search-registries {:keyword "movies"}
+                         [:registry/address
+                          :registry/title
+                          :registry/description]]]})
+
+  )
+======= end
