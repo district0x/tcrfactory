5283112fac2d437140c815a20e2baaa61d7518c7
Mike Konkov <noospheratum@gmail.com>
2018-05-26 20:18:54 +0200

    Some more styles

--- src/tcrfactory/shared/smart_contracts.cljs
+++ src/tcrfactory/shared/smart_contracts.cljs
@@ -6,16 +6,16 @@
   :address "0x148b2fd1d764c72351e548eb602d26afc8dfcac2"},
  :minime-token-factory
  {:name "MiniMeTokenFactory",
-  :address "0xe6373adf8c5be5b98b3bbe71f3dc9888fbfec2a5"},
+  :address "0x285bb01dfcc648bd450c350a6357f82ee89909df"},
  :registry-entry
  {:name "RegistryEntry",
-  :address "0xeaa447cf66814785665f14701f75a5911b67bbdb"},
+  :address "0xf05530e5696840d8d89fe0dc685e3b9cb9c1ab55"},
  :registry
  {:name "Registry",
-  :address "0xce7077b08f6d12d27f22e2e0c3d8e71c6e4d98e1"},
+  :address "0xfacc24d130a8b76f4e46e041dd063a2aeb873d01"},
  :registry-factory
  {:name "RegistryFactory",
-  :address "0x96b64c9975640c945d10320dd702b51492ba993d"},
+  :address "0x81c89ff869dbe223106d5aaa1529446e7f784913"},
  :registry-entry-factory
  {:name "RegistryEntryFactory",
   :address "0x0000000000000000000000000000000000000000"}})
